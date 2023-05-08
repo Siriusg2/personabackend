3c69164306bcbc9527f4c7ba45d73441829b4f4b
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-05-08 13:26:21 -0500

    route to get all states is working

--- src/main/java/com/consulti/personas/personabackend/rest/EstadoRest.java
+++ src/main/java/com/consulti/personas/personabackend/rest/EstadoRest.java
@@ -1,11 +1,27 @@
 package com.consulti.personas.personabackend.rest;
 
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.consulti.personas.personabackend.model.*;
+
+import com.consulti.personas.personabackend.service.EstadoService;
 
 @RestController
 @RequestMapping("/estado/")
 public class EstadoRest {
-    
+    @Autowired
+    private EstadoService estadoService;
+
+    @GetMapping
+    private ResponseEntity<List<Estado>> getAllEstados(){
+        return ResponseEntity.ok( estadoService.findAll());
+    }
+
 }
